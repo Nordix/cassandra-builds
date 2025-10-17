5af91c456f0833b019f6d3f2b93001fdb503f4af
Vinnie McGuinness <vinnie.mcguinness@intive.com>
2025-10-17 14:13:49 +0100

    Update cassandra_job_dsl_seed.groovy to support Nordix fork and parameter overrides 5

--- jenkins-dsl/cassandra_job_dsl_seed.groovy
+++ jenkins-dsl/cassandra_job_dsl_seed.groovy
@@ -1226,7 +1226,8 @@ matrixJob('Cassandra-devbranch-before-5-cqlsh-tests') {
     parameters {
         stringParam('REPO', 'apache', 'The github user/org to clone cassandra repo from')
         stringParam('BRANCH', 'cassandra-4.1', 'The branch of cassandra to checkout, must be based off before cassandra-5.0')
-        stringParam('DTEST_REPO', "${dtestRepo}", 'The cassandra-dtest repo URL')
+        // stringParam('DTEST_REPO', "${dtestRepo}", 'The cassandra-dtest repo URL')
+        stringParam('DTEST_REPO', "https://github.com/apache/cassandra-dtest", 'The cassandra-dtest repo URL')
         stringParam('DTEST_BRANCH', 'trunk', 'The branch of cassandra-dtest to checkout')
     }
     axes {
