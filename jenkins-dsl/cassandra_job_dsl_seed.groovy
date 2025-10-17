6df1c057c75c89f0e83949855ca866a3f77a01cc
Vinnie McGuinness <vinnie.mcguinness@intive.com>
2025-10-17 13:48:24 +0100

    Update cassandra_job_dsl_seed.groovy to support Nordix fork and parameter overrides 2

--- jenkins-dsl/cassandra_job_dsl_seed.groovy
+++ jenkins-dsl/cassandra_job_dsl_seed.groovy
@@ -28,12 +28,10 @@ def slaveDtestLargeLabel = 'cassandra-dtest-large'
 def slaveArm64Label = 'cassandra-arm64'
 def slaveArm64DtestLabel = 'cassandra-arm64-dtest'
 def slaveArm64DtestLargeLabel = 'cassandra-arm64-dtest-large'
-def mainRepo = "https://github.com/apache/cassandra"
 
 def mainRepo   = binding.hasVariable('MAIN_REPO')   ? MAIN_REPO   : "https://github.com/nordix/cassandra"
 def buildsRepo = binding.hasVariable('BUILDS_REPO') ? BUILDS_REPO : "https://github.com/nordix/cassandra-builds"
 def buildsBranch = binding.hasVariable('BUILDS_BRANCH') ? BUILDS_BRANCH : "trunk"
-
 
 def dtestBranch = "trunk"
 def buildDescStr = 'REF = ${GIT_BRANCH} <br /> COMMIT = ${GIT_COMMIT}'
